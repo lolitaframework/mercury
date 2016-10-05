81fe95f5d96cb991ea4f00d5246106cfa3493a90
vitalik1921 <vitalik1921@gmail.com>
2016-10-05 12:43:51 +0300

    b-back-to-top, b-about-me, c-aside-title, c-portrati

---
+++ app/modules/b-back-to-top/js/b-back-to-top.js
@@ -0,0 +1,24 @@
+$b_back_to_top = jQuery('.b-back-to-top');
+$scrollTrigger = 100;
+
+back_to_top = function() {
+	var scrollTop = $(window).scrollTop();
+	if (scrollTop > scrollTrigger) {
+		$b_back_to_top.addClass('b-back-to-top--visible');
+	} else {
+		$b_back_to_top.removeClass('b-back-to-top--visible');
+	}
+}
+
+jQuery(window).on('scroll', 
+	function() {
+		back_to_top();
+	}
+);
+
+$b_back_to_top.on('click',
+    function(e) {
+    	e.preventDefault();
+    	jQuery('html, body').animate({scrollTop: 0} , 700);
+    }
+);

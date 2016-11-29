067ca4b7a6dc5cf822c72449ccc25e01f552976c
Vitaliy Shebela <vitalik.privat@gmail.com>
2016-11-29 13:42:46 +0200

    Refactored

--- app/general/ts/settings.ts
+++ app/general/ts/settings.ts
@@ -3,9 +3,9 @@
 
 namespace LolitaFramework {
 	// media breakpoints
-	LolitaFramework.MediaBreakpoints.addMediaBreakpoin({ name: 'extra', upValue: 9999});
-	LolitaFramework.MediaBreakpoints.addMediaBreakpoin({ name: 'wide', upValue: 1200});
-	LolitaFramework.MediaBreakpoints.addMediaBreakpoin({ name: 'small', upValue: 1024});
-	LolitaFramework.MediaBreakpoints.addMediaBreakpoin({ name: 'tablets', upValue: 768});
-	LolitaFramework.MediaBreakpoints.addMediaBreakpoin({ name: 'phones', upValue: 420});
+	MediaBreakpoints.addMediaBreakpoin({ name: 'extra', upValue: 9999});
+	MediaBreakpoints.addMediaBreakpoin({ name: 'wide', upValue: 1200});
+	MediaBreakpoints.addMediaBreakpoin({ name: 'small', upValue: 1024});
+	MediaBreakpoints.addMediaBreakpoin({ name: 'tablets', upValue: 768});
+	MediaBreakpoints.addMediaBreakpoin({ name: 'phones', upValue: 420});
 }
--- app/modules/b-search/ts/b-search.ts
+++ app/modules/b-search/ts/b-search.ts
@@ -3,6 +3,11 @@
 
 namespace LolitaFramework.Blocks {
     export class Search extends LolitaFramework.Block {
+        /**
+         * Current Item
+         * @type {JQuery}
+         */
+        public currentItem: JQuery;
         
         /**
          * Constructor
@@ -13,8 +18,59 @@ namespace LolitaFramework.Blocks {
 
             this.block.each(
                 (index, item) => {
-                    let closeButton = jQuery(item).find('.b-search__close');
+                    let block = jQuery(item);
+                    let closeButton = block.find('.b-search__close');
                     closeButton.on('click', () => { this.closeClick(); });
+
+                    // active/nonactive search-block for keypresses
+                    block.on('mouseenter', 
+                        () => {
+                             block.addClass('b-search--active');
+                        }
+                    );
+
+                    jQuery(document).on('mouseup',
+                        (event) => {
+                            if (!block.is(event.target) 
+                                && block.has(event.target).length === 0) {
+                                block.removeClass('b-search--active');
+                            }
+                        }
+                    );
+
+                    // keypresses
+                    jQuery(document).keydown(
+                        (event) => {
+                            if (block.hasClass('b-search--active')) {
+                                switch (event.which) {
+                                    case 38: // up
+                                        event.preventDefault();
+                                        this.prevItem(block);
+                                        break;
+
+                                    case 40: // down
+                                        event.preventDefault();
+                                        this.nextItem(block);
+                                        break;
+
+                                    default:
+                                        return;
+                                }
+                            }
+                        }
+                    );
+
+                    // mouse hover
+                    let itemsContainer = block.find('.b-search__results');
+                    itemsContainer.on('mouseenter', '.b-search__item',
+                        (event) => {
+                            let items = itemsContainer.find('.b-search__item');
+                            let currentItem = jQuery(event.currentTarget);
+                            items.removeClass('c-search-item--active');
+                            currentItem.addClass('c-search-item--active');
+                        }
+                    );
+
                 }
             );
         }
@@ -22,8 +78,38 @@ namespace LolitaFramework.Blocks {
         /**
          * Close click
          */
-        private closeClick() {
+        public closeClick() {
         	jQuery('body').trigger('b-search__close::click');
+        }
+
+        /**
+         * Set prev Item
+         * @param {JQuery} block current block
+         */
+        public prevItem(block: JQuery) {
+            let items = block.find('.b-search__item');
+            let prevItem = items.filter('.c-search-item--active').prev();
+
+            items.removeClass('c-search-item--active');
+            if (!prevItem.length) {
+                prevItem = items.last();
+            }
+            prevItem.addClass('c-search-item--active');
+        }
+
+        /**
+         * Set next Item
+         * @param {JQuery} block current block
+         */
+        public nextItem(block: JQuery) {
+            let items = block.find('.b-search__item');
+            let prevItem = items.filter('.c-search-item--active').next();
+
+            items.removeClass('c-search-item--active');
+            if (!prevItem.length) {
+                prevItem = items.first();
+            }
+            prevItem.addClass('c-search-item--active');
         }
     }
 
